9d4d31df00b7c48513cfe05cdbccb46c6efebc80
Thomas Pfeifer <git@thomaspfeifer.net>
2010-10-02 10:28:27 +0200

    rewrite of the ppm-decoder

--- firmware/ppm.c
+++ firmware/ppm.c
@@ -22,54 +22,84 @@
 //=========================================================================
 
 #include <avr/io.h>
-#include <avr/interrupt.h>
 #include "ppm.h"
+#include <avr/interrupt.h>
 
 #define PPM_TCNT TCNT1
 
 #define CHANNELS (8)
 volatile unsigned int ppm[CHANNELS];
-volatile char chan=-1;
+volatile int chan=-1;
+
+#define T_MIN (1*F_CPU/1000) // 1ms = 12000 Ticks @ 12Mhz = Stick min position
+#define T_MAX (2*F_CPU/1000) // 2ms = 24000 Ticks @ 12Mhz = Stick max position
+#define T_OUT (3*F_CPU/1000) // 3ms = 36000 Ticks @ 12Mhz = Timeout
 
 volatile char ppmNewData;
+volatile unsigned int timeOld;
 
-SIGNAL (SIG_OVERFLOW1) {
+/**
+* Interrupt: Compare 1A match = timeout
+*/
+SIGNAL (SIG_OUTPUT_COMPARE1A) {
   if (chan!=-1) {
     ppmNewData=1;
   }
   chan=-1;
 }
 
+/**
+* Interrupt: ICP-Edge detect
+*/
 SIGNAL (SIG_INPUT_CAPTURE1) {
-  unsigned int timer=PPM_TCNT;
-  PPM_TCNT=0;
+  unsigned int time=ICR1;
+  OCR1A=time+T_OUT;
+  if (time>timeOld) {
+    time=time-timeOld;
+  } else {
+    time=time+(0xffff-timeOld)+1;
+  }
+  timeOld=ICR1;
   if (chan>=0 && chan<CHANNELS) {
-    ppm[chan]=timer;
+    ppm[chan]=time;
   }
   chan++;
 }
 
-
+/**
+* Init Timer1
+*/
 void ppmInit() {
-#ifdef TIMSK
-  // tested with ATMega8
-  TCCR1B=(1<<ICES1)|(1<<CS10); // TClk=CPUClk (no prescaler), ICP on rising edge
-  TIMSK=(1<<TICIE1)|(1<<TOIE1); //Overflow and ICP-Interrupt enable
-#else
-  // for ATMega48/ATMega88/ATMega168
-  TCCR1B=(1<<ICES1)|(1<<CS10); // TClk=CPUClk (no prescaler), ICP on rising edge
-  TIMSK1=(1<<ICIE1)|(1<<TOIE1); //Overflow and ICP-Interrupt enable
+
+#ifndef TIMSK
+  // for Mega48/88/168 (not tested with hardware!)
+  #define TICIE1 ICIE1
+  #define TIMSK TIMSK1
 #endif
+
+  // tested with ATMega8
+  TCCR1B=(1<<CS10); // TClk=CPUClk (no prescaler)
+  TCCR1B|=(1<<ICNC1); // enable input noise chanceler
+  TCCR1B|=(1<<ICES1); // positive edge (comment out for inverted signals)
+  //ACSR|=(1<<ACIC); // use analog comparator instead of icp-pin
+  TIMSK=(1<<TICIE1)|(1<<OCIE1A); // ICP-Interrupt and Output Compare A Match enable
+
   ppmNewData=0;
+  chan=-1;
 }
 
+/**
+* Get PPM-Channel n (8-bit resolution)
+*/
 unsigned char ppmGet(int n) {
   unsigned int t=ppm[n];
-  if (t<12000) t=12000;
-  t-=12000;
-  //t=(u16)((255.0/12000.0)*(float)t); //slow!
-  t=t/45;
+
+  // conversion T_MIN...T_MAX -> 0...255
+  if (t<T_MIN) t=T_MIN;
+  t-=T_MIN;
+  t=t/((T_MAX-T_MIN)/255);
   if (t>255) t=255;
+
   return ((unsigned char)t);
 }
 
